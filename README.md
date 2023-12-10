eae5ddfae6b05a7b303b6eca1f9930769133cbe3
Priyanshu9898 <priyanshu.mce20@sot.pdpu.ac.in>
2023-12-10 12:02:42 +0530

    setup.py is created along with environment creation and library installations

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # Chest-Cancer-Classification
 CT Scan Chest Cancer Classification using Deep learning, Transformers, mlflow, DVC,  AWS
+
+## Workflows
+
+1. Update config.yaml
+2. Update secrets.yaml [Optional]
+3. Update params.yaml
+4. Update the entity
+5. Update the configuration manager in src config
+6. Update the components
+7. Update the pipeline 
+8. Update the main.py
+9. Update the dvc.yaml
+10. app.py
+
+# How to run?
+### STEPS:
+
+Clone the repository
+
+```bash
+git clone https://github.com/Prriyanshu9898/Chest-Cancer-Classification
+```
+### STEP 01- Create a Python environment after opening the repository
+
+```bash
+python -m venv env
+```
+
+```bash
+env\Scripts\activate
+```
+
+
+### STEP 02- install the requirements
+```bash
+pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,29 @@
+import os
+import sys
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Chest-Cancer-Classification"
+AUTHOR_USER_NAME = "Priyanshu9898"
+SRC_REPO = "chestCancerClassification"
+AUTHOR_EMAIL = "priyanshumalaviya9210@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for Chest-Cancer-Classification",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
