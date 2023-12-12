b12115c0b9af4c89bb7863068cc6e0e3b4bda569
Priyanshu9898 <priyanshu.mce20@sot.pdpu.ac.in>
2023-12-12 11:45:15 +0530

    stage 1 - data ingestion pipeline is created

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from chestCancerClassification import logger
+from chestCancerClassification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("welcome to chest classifications")
+STAGE_NAME = "Data Ingestion stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
---
+++ src/chestCancerClassification/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,17 @@
+from chestCancerClassification.config.configuration import ConfigurationManager
+from chestCancerClassification.components.data_ingestion import DataIngestion
+from chestCancerClassification import logger
+
+
+STAGE_NAME = "Data Ingestion stage"
+
+class DataIngestionTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+        data_ingestion.download_file()
+        data_ingestion.extract_zip_file()
