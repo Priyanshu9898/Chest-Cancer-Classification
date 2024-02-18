0697b95b537079f38fd4aa45300a6ab333740ada
Priyanshu9898 <priyanshu.mce20@sot.pdpu.ac.in>
2024-02-18 23:54:36 +0530

    frontend urls added

--- app.py
+++ app.py
@@ -12,8 +12,10 @@ from PIL import Image
 app = Flask(__name__)
 CORS(app)
 
-allowed_origins = ['https://meta-doctor.vercel.app', 'https://meta-doctor-git-main-priyanshumalaviya9210-gmailcom.vercel.app', 'https://meta-doctor-nwxeblfpx-priyanshumalaviya9210-gmailcom.vercel.app', 'https://oral-disease-classification.vercel.app', 'http://localhost:3000', 'http://127.0.0.1:3000',
-                   'https://oral-disease-classification.vercel.app/']
+# Define allowed origins for CORS
+allowed_origins = ["https://metadoctor.vercel.app/", "https://meta-doctor.vercel.app", "https://meta-doctor.vercel.app/", "https://metadoctor-git-main-priyanshumalaviya9210-gmailcom.vercel.app/",  "https://metadoctorhelper.vercel.app", "http://localhost:3000",
+                   "http://127.0.0.1:3000", "https://metadoctorhelper.vercel.app", "https://metadoctor-priyanshumalaviya9210-gmailcom.vercel.app/", "https://meta-doctor.vercel.app", "https://meta-doctor-nwxeblfpx-priyanshumalaviya9210-gmailcom.vercel.app", "https://meta-doctor-git-main-priyanshumalaviya9210-gmailcom.vercel.app"]
+
 
 
 # Function to load the model (modify according to your needs)
